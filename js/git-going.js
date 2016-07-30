a6e96ee1fb1cba5b4e1b3dc22a8b3e1d66f8c4bc
James Milner <jamesmilner22@hotmail.com>
2016-07-30 13:37:57 +0100

    MVP search functionality

--- js/git-going.js
+++ js/git-going.js
@@ -1,13 +1,66 @@
+var CommandsList = [
+
+  {
+     "use" : "Initialise a git repository",
+     "explanation" : "This command creates a repository in a folder which hasn't previously been initialised as a git repository",
+     "command" : "git init",
+     "complexity" : "0",
+     "tags" : ["init", "initialise", "start", "create"]
+   },
+   {
+     "use" : "Add files to be staged",
+     "explanation" : "",
+     "command" : "git add .",
+     "complexity" : "0",
+     "tags" : ["initialise", "start", "create"]
+   },
+
+   {
+     "use" : "Copy (clone) a remote repository locally to your machine",
+     "explanation" : "",
+     "command" : "git clone https://github.com/JamesMilnerUK/git-going.git",
+     "complexity" : "0",
+     "tags" : ["initialise", "start", "create"]
+   }
+
+];
+
+var Store = {
+
+  state : {
+    searchInput : "",
+    searched : false,
+    commands: CommandsList
+  },
+
+  textSearch : function() {
+    this.state.searched = true;
+  },
+
+  blankSearch : function() {
+    this.state.searched = false;
+  }
+
+
+};
 
 var Search = new Vue({
 
   el: '#search',
-  data: {
-    searchInput : ""
-  },
+  data: Store.state,
   methods: {
     onSubmit: function(){
-      console.log(Search.$data.searchInput);
+      this.handleInput();
+    },
+    onKeyUp: function() {
+      this.handleInput();
+    },
+    handleInput : function() {
+      if (this.searchInput) {
+        Store.textSearch();
+      } else {
+        Store.blankSearch();
+      }
     }
   }
 
@@ -16,52 +69,20 @@ var Search = new Vue({
 var Commands = new Vue({
 
   el: '#commands',
+  data: Store.state,
   methods: {
     show : function(command) {
 
-      console.log("hello");
-      //
-      // function matches(value) {
-      //   return value == Search.searchInput;
-      // }
-      //
-      // return commands.filter(matches);
-      //
-      // console.log(command);
+      var searched = this.searched;
+      var tags = command.tags.indexOf(this.searchInput) !== -1;
+      return !searched || (searched && tags);
+
+    },
+    matching : function() {
+
       return true;
+
     }
-  },
-  data: {
-    commands: [
-      {
-         "use" : "Initialise a git repository",
-         "explanation" : "This command creates a repository in a folder which hasn't previously been initialised as a git repository",
-         "command" : "git init",
-         "complexity" : "0",
-         "tags" : ["initialise", "start", "create"]
-       },
-       {
-         "use" : "Add files to be staged",
-         "explanation" : "",
-         "command" : "git add .",
-         "complexity" : "0",
-         "tags" : ["initialise", "start", "create"]
-       },
-
-       {
-         "use" : "Copy (clone) a remote repository locally to your machine",
-         "explanation" : "",
-         "command" : "git clone https://github.com/JamesMilnerUK/git-going.git",
-         "complexity" : "0",
-         "tags" : ["initialise", "start", "create"]
-       }
-
-    ]
   }
 
 });
-
-// Vue.filter('search', function (command) {
-//
-//
-// });
